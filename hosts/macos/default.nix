46cb5c7609787d50854c3bb892b14b734ccc05f9
dfjay <mail@dfjay.com>
2025-10-16 12:13:18 +0500

    Add bruno, windows-app (macos)

--- hosts/macos/default.nix
+++ hosts/macos/default.nix
@@ -69,6 +69,7 @@
       "balenaetcher"
       "beekeeper-studio"
       "bitwarden"
+      "bruno"
       "claude"
       "cyberduck"
       "datagrip"
@@ -108,6 +109,7 @@
       "visualvm"
       "vmware-fusion"
       "waterfox"
+      "windows-app"
       "yubico-authenticator"
       "zotero"
     ];
