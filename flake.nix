e6f707b2c03e62619916ebfcc3d399af15a9c494
dfjay <mail@dfjay.com>
2025-11-15 13:21:33 +0500

    Remove hyprland

--- flake.nix
+++ flake.nix
@@ -26,8 +26,6 @@
 
     preservation.url = "github:nix-community/preservation";
 
-    hyprland.url = "github:hyprwm/Hyprland";
-
     mac-app-util.url = "github:hraban/mac-app-util";
 
     nix-flatpak.url = "github:gmodena/nix-flatpak";
@@ -36,6 +34,7 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nix4nvchad = {
       url = "github:nix-community/nix4nvchad";
       inputs.nixpkgs.follows = "nixpkgs";
