d9c0f874612cd6d8bba20efe38a0283a847e33a4
dfjay <mail@dfjay.com>
2026-04-03 01:20:13 +0500

    Disable cursor blink ghostty

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -6,6 +6,9 @@
       programs.ghostty = {
         enable = true;
         package = if pkgs.stdenv.isDarwin then null else pkgs.ghostty;
+        settings = {
+          cursor-style-blink = false;
+        };
       };
     };
 
