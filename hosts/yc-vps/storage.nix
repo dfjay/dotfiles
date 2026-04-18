7ba0f7c90671fff9eb85bfd73793fc45cfb3e5d5
dfjay <mail@dfjay.com>
2026-04-18 23:08:27 +0500

    Add tcp bbr for yc vps

--- hosts/yc-vps/storage.nix
+++ hosts/yc-vps/storage.nix
@@ -3,6 +3,13 @@
 {
   boot.loader.grub.device = "/dev/vda";
 
+  # TCP BBR for better throughput
+  boot.kernel.sysctl = {
+    "net.core.default_qdisc" = "fq";
+    "net.ipv4.tcp_congestion_control" = "bbr";
+  };
+  boot.kernelModules = [ "tcp_bbr" ];
+
   fileSystems."/" = {
     device = "/dev/vda2";
     fsType = "ext4";
