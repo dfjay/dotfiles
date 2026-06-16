c109eef0fab5611898f957519a1657f15861838b
dfjay <mail@dfjay.com>
2026-06-16 17:39:36 +0500

    chore(deps): update lanzaboote input

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
